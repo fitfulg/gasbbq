cdd25892d7dc26759f0ac5adc0701f31794e908e
Eric <rc84.eric@gmail.com>
2024-09-05 07:27:52 +0200

    Remove SheetModel

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,9 @@
 // Simulates the global scope of the Google Apps Script environment
 
-global.SheetModel = require('./src/models/SheetModel').SheetModel;
 global.SheetController = require('./src/controllers/SheetController').SheetController;
 global.EventController = require('./src/controllers/EventController').EventController;
 global.SheetService = require('./src/services/SheetService').SheetService;
 global.ValidationService = require('./src/services/ValidationService').ValidationService;
 global.ProtectionService = require('./src/services/ProtectionService').ProtectionService;
 global.WordCountService = require('./src/services/WordCountService').WordCountService;
-global.Utils = require('./src/utils/Utils'); 
+global.Utils = require('./src/utils/Utils').Utils;
