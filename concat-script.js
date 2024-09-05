74efc4a6ac00b4a916ecf8d8705fd54b28cb8301
Eric <rc84.eric@gmail.com>
2024-09-05 10:19:46 +0200

    Rename ValidationService to DropdownService for better clarity

--- concat-script.js
+++ concat-script.js
@@ -120,6 +120,20 @@ class SheetService {
 
 
 
+class DropdownService {
+    constructor(sheet) {
+        Logger.log('DropdownService  constructor called');
+        this.sheet = sheet;
+    }
+
+    applyConfirmationValidation() {
+        Logger.log('applyConfirmationValidation called');
+        const confirmRange = this.sheet.getRange('B2:B45');
+        const rule = SpreadsheetApp.newDataValidation().requireValueInList(['Sí', 'No'], true).build();
+        confirmRange.setDataValidation(rule);
+        this.sheet.getRange('B2:B45').setBorder(true, true, true, true, true, true);
+    }
+}
 
 
 class ProtectionService {
