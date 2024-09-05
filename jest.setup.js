5e0356c2bae0e8d51f35c6db85412838068e160f
Eric <rc84.eric@gmail.com>
2024-09-05 08:47:11 +0200

    Setup GitHub Actions workflow for GAS deployment with clasp

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-// Simulates the global scope of the Google Apps Script environment
-
-global.SheetController = require('./src/controllers/SheetController').SheetController;
-global.EventController = require('./src/controllers/EventController').EventController;
-global.SheetService = require('./src/services/SheetService').SheetService;
-global.ValidationService = require('./src/services/ValidationService').ValidationService;
-global.ProtectionService = require('./src/services/ProtectionService').ProtectionService;
-global.WordCountService = require('./src/services/WordCountService').WordCountService;
-global.Utils = require('./src/utils/Utils').Utils;
